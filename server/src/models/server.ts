b1da9a55660db660faafa0736638854f43a165ac
Jorge Cervantes <123649334+Georggge420@users.noreply.github.com>
2024-10-15 20:17:09 -0600

    agregando configuraciones para el servidor

--- server/src/models/server.ts
+++ server/src/models/server.ts
@@ -1,45 +1,58 @@
-import express, { Application } from 'express';
-import cors from 'cors';
+import express, {Application, Request, Response} from 'express';
 import routesPlayer from '../routes/player';
+import cors from 'cors';
 import db from '../db/coneccion';
 
-export class Server {
+export class Server{
     private app: Application;
     private port: string;
-
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT || '3001'; // Asegúrate de que este puerto esté configurado correctamente
-        this.middlewares(); // Aquí se configura CORS
-        this.routes();
-        this.dbConnect();
+    
+    constructor(){
+            console.log(process.env.PORT);
+            this.app = express();
+            this.port = process.env.PORT || '3001';
+            this.midlewares();
+            this.listen();
+            this.routes();
+            this.dbConnect();
     }
 
-    middlewares() {
-        // Permitir todas las orígenes (uso temporal solo en desarrollo)
-        this.app.use(cors()); // Aquí es donde agregas esta línea
-
-        // Parsear el body a JSON
-        this.app.use(express.json());
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log(`Aplicacion corriendo en el puerto ${this.port}`);
+        })
     }
 
-    routes() {
-        // Definir las rutas aquí
+    routes(){
+        this.app.get('/', (req: Request, res: Response) => {
+            res.json({
+                msg: 'API working'
+            })
+        })
+
         this.app.use('/api/players', routesPlayer);
     }
 
-    async dbConnect() {
-        try {
-            await db.authenticate();
-            console.log('Base de datos conectada');
-        } catch (error) {
-            console.log('Error al conectarse a la base de datos:', error);
-        }
+    midlewares(){
+
+        this.app.use(cors({
+            origin: 'http://localhost:4200' // Cambia esto por la dirección de tu frontend si es diferente
+        }));
+
+        //Parsear el body
+        this.app.use(express.json());
     }
 
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Servidor corriendo en el puerto ${this.port}`);
-        });
+    async dbConnect(){
+
+        try {
+            await db.authenticate();
+            console.log("base de datos conectada");
+        } catch (error) {
+            console.log(error);
+            console.log('Error al conectarse con la base de datos');
+        }
+
+        
     }
 }
